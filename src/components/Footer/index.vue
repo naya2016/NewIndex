58fe492bf53a8dffb1cdf5178ad8d1e93904d63a
naya2016 <63491927+naya2016@users.noreply.github.com>
2023-05-03 08:35:06 +0000

    改掉版权信息

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -3,7 +3,7 @@
     <div class="power" v-show="!store.playerState">
       <span
         >Copyright&nbsp;&copy;&nbsp;{{ fullYear }}
-        <a href="https://imsyy.top">無名</a>
+        <a href="https://www.lizonglin.site">纳雅（参宿璀璨）</a>
       </span>
       <!-- 以下信息请不要修改哦 -->
       <span class="hidden"
@@ -15,8 +15,8 @@
         </a></span
       >&nbsp;&amp;
       <!-- 站点备案 -->
-      <a href="https://beian.miit.gov.cn" target="_blank"
-        >豫ICP备2022018134号-1</a
+      <a href="" target="_blank"
+        >备个屁的案~</a
       >
     </div>
     <div class="lrc" v-show="store.playerState">
